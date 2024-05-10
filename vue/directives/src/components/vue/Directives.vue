76016df9a4fd1c107fdda635e02d978d45261069
SCIS2 <ymmestram@ufpso.edu.co>
2024-05-10 18:04:36 -0500

    clase vue 2

---
+++ vue/directives/src/components/vue/Directives.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+
+const users = ["Pepe","Ana","Juana","Lola"]
+
+const isChecked = ref(false)
+
+</script>
+<template>
+    <ul>
+        <li v-for="(user, index) in users" :key="index">{{ user }}</li>
+    </ul>
+
+    <input type="checkbox" id="check" class="p-2" v-model="isChecked"/>
+    <label for="check">Pinchame</label>
+    <div class="bg-blue-400 text-white rounded py-1 px-2"
+    :class="{ 'bg-orange-500': isChecked}"
+    > este es un mensaje
+    </div>
+    <div v-show="isChecked">
+        Este es un mensaje que podria ocultar 
+        v-model solo para formularios 
+        : para cualquier propiedad de una etiqueta
+    </div>
+    <div v-if="isChecked">
+        Este es un mensaje que podria ocultar 
+    </div>
+    <div v-else>Esto se ve</div>
+</template>
---
+++ vue/directives/src/components/vue/TODO.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const todos = ref(['Estudiar bastante'])
+
+const todo = ref('')
+
+function add(){
+    todos.value.unshift(todo.value)
+    todo.value = ''
+}
+
+</script>
+<template>
+    <div class="w-96 bg-orange-200 m-auto mt-7 rounded-lg shadow-xl px-2 py-3">
+        <div>
+            <input v-model="todo" class="w-4/5 rounded outline-none px-2"/>
+            <button @click="add" class="rounded w-1/5 bg-green-600 text-white text-center">+</button>
+        </div>
+        <div class="mt-3">
+            <div v-for="(todo, index) in todos" :key="index" class="bg-orange-300 rounded mt-1">
+                {{ todo }}
+            </div>
+        </div>
+    </div>
+</template>
